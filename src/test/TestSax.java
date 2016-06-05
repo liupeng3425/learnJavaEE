a443e3ccc0a83037b7981dfedd42fc9d860105ac
lp5 <liupeng3425@qq.com>
2016-06-05 11:24:52 +0800

    Initial commit

---
+++ src/test/TestSax.java
@@ -0,0 +1,23 @@
+package test;
+
+import org.xml.sax.SAXException;
+import utils.XmlDecoder;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by Peng on 2016/6/5.
+ * test sax
+ */
+public class TestSax {
+    public static void main(String[] args) {
+        File people = new File("src/xml/people.xml");
+        try {
+            XmlDecoder.sax(people);
+        } catch (ParserConfigurationException | IOException | SAXException e) {
+            e.printStackTrace();
+        }
+    }
+}
