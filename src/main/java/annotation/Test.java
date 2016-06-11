99217c843ec961beb39f2cea4f13e80f5e9292f6
lp5 <liupeng3425@qq.com>
2016-06-11 15:49:04 +0800

    java 注解解析demo

--- src/main/java/annotation/Test.java
+++ src/main/java/annotation/Test.java
@@ -11,17 +11,26 @@ import java.lang.reflect.Method;
 public class Test {
     public static void main(String[] args) {
         try {
-            Class mClass = Class.forName("annotation.Child");
-            Method method = mClass.getDeclaredMethod("eatFood");
+            // 要将Class改成 Class<?> 否则getMethod() 报警告。
+//            Class mClass = Class.forName("annotation.Child");
+            Class<?> mClass = Class.forName("annotation.Child");
+            Method method = mClass.getMethod("eatFood");
+
+            // 第一种方法，先判断有没有Description注解，然后直接获取注解实例
+            boolean isExist  = method.isAnnotationPresent(AnnotationDemo.Description.class);
+            if (isExist) {
+                AnnotationDemo.Description description = method.getAnnotation(AnnotationDemo.Description.class);
+                System.out.println(description.value());
+            }
+
+            // 第二种方法，获取方法上所有注解，遍历。
             Annotation[] mAnnotation = method.getAnnotations();
             for (Annotation annotation:mAnnotation){
                 if (annotation instanceof AnnotationDemo.Description){
                     System.out.println(((AnnotationDemo.Description) annotation).value());
                 }
             }
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (NoSuchMethodException e) {
+        } catch (ClassNotFoundException | NoSuchMethodException e) {
             e.printStackTrace();
         }
     }
